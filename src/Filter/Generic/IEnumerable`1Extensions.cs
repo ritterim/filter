82f49798bc03887357d4be861fb73610f19e046e
William Boga IV <bill.boga@ritterim.com>
2015-05-06 12:12:58 -0400

    Add filter project and related tests. This is the core of the library's function. An IEnumerable of some class-type can be filtered by using a class with properties and values. As long as the the filter property-value type matches the related property on the IEnumerable-type everything is good. Currently, there are a few fixed-types which work with the filter. Specifically on the Range-type, only ints are allowed.

---
+++ src/Filter/Generic/IEnumerable`1Extensions.cs
@@ -0,0 +1,130 @@
+﻿using RimDev.Filter.Range.Generic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace RimDev.Filter.Generic
+{
+    public static class IEnumerable_1Extensions
+    {
+        public static IEnumerable<T> Filter<T>(
+            this IEnumerable<T> value,
+            object filter)
+        {
+            var validValueProperties = typeof(T).GetProperties(
+                BindingFlags.Public | BindingFlags.Instance)
+                .Where(x => x.CanRead == true);
+
+            var filterProperties = filter.GetType().GetProperties(
+                BindingFlags.Public | BindingFlags.Instance)
+                .Where(x => x.CanRead == true);
+
+            var queryableValue = value.AsQueryable();
+
+            foreach (var filterProperty in filterProperties)
+            {
+                var validValueProperty = validValueProperties
+                    .Where(x => x.Name == filterProperty.Name)
+                    .FirstOrDefault();
+                var filterPropertyValue = filterProperty.GetValue(filter, null);
+
+                if (validValueProperty != null)
+                {
+                    var validValuePropertyName = validValueProperty.Name;
+
+                    if (typeof(IEnumerable).IsAssignableFrom(filterProperty.PropertyType) &&
+                        filterProperty.PropertyType != typeof(string))
+                    {
+                        queryableValue = queryableValue.Contains(validValuePropertyName, (IEnumerable)filterPropertyValue);
+                    }
+                    else if (typeof(Range<int>).IsAssignableFrom(filterProperty.PropertyType))
+                    {
+                        queryableValue = queryableValue.Range(validValuePropertyName, (Range<int>)filterPropertyValue);
+                    }
+                    else
+                    {
+                        try
+                        {
+                            queryableValue = queryableValue.Where(validValuePropertyName, filterPropertyValue);
+                        }
+                        catch (Exception)
+                        { }
+                    }
+                }
+            }
+
+            return queryableValue.AsEnumerable();
+        }
+
+        private static IQueryable<T> Contains<T>(
+            this IQueryable<T> query,
+            string property,
+            IEnumerable values)
+        {
+            var parameterExpression = Expression.Parameter(typeof(T), "x");
+            var propertyExpression = Expression.Property(parameterExpression, property);
+            var constantExpression = Expression.Constant(values);
+            var callExpression = Expression.Call(
+                typeof(Enumerable),
+                "Contains",
+                new[] { propertyExpression.Type },
+                constantExpression,
+                propertyExpression);
+            var lambda = Expression.Lambda<Func<T, bool>>(callExpression, parameterExpression);
+
+            return query.Where(lambda);
+        }
+
+        private static IQueryable<T> Where<T>(
+            this IQueryable<T> query,
+            string property,
+            object value)
+        {
+            var parameterExpression = Expression.Parameter(typeof(T), "x");
+            var propertyExpression = Expression.Property(parameterExpression, property);
+            var constantExpression = Expression.Constant(value);
+            var equalExpression = Expression.Equal(propertyExpression, constantExpression);
+            var callExpression = Expression.Call(
+                typeof(Queryable),
+                "Where",
+                new[] { query.ElementType },
+                query.Expression,
+                Expression.Lambda<Func<T, bool>>(equalExpression, new[] { parameterExpression }));
+
+            return query.Provider.CreateQuery<T>(callExpression);
+        }
+
+        private static IQueryable<T> Range<T, TRange>(
+            this IQueryable<T> query,
+            string property,
+            Range<TRange> range)
+        {
+            var parameterExpression = Expression.Parameter(typeof(T), "x");
+            var propertyExpression = Expression.Property(parameterExpression, property);
+
+            var minConstantExpression = Expression.Constant(range.MinValue);
+            BinaryExpression minGreaterExpression = range.IsMinInclusive
+                ? Expression.GreaterThanOrEqual(propertyExpression, minConstantExpression)
+                : Expression.GreaterThan(propertyExpression, minConstantExpression);
+
+            var maxConstantExpression = Expression.Constant(range.MaxValue);
+            BinaryExpression maxLessExpression = range.IsMaxInclusive
+                ? Expression.LessThanOrEqual(propertyExpression, maxConstantExpression)
+                : Expression.LessThan(propertyExpression, maxConstantExpression);
+
+            var logicExpression = Expression.And(minGreaterExpression, maxLessExpression);
+
+            var callExpression = Expression.Call(
+                typeof(Queryable),
+                "Where",
+                new[] { query.ElementType },
+                query.Expression,
+                Expression.Lambda<Func<T, bool>>(logicExpression, new[] { parameterExpression }));
+
+            return query.Provider.CreateQuery<T>(callExpression);
+        }
+    }
+}
---
+++ tests/Filter.Tests/Generic/IEnumerable`1ExtensionsTests.cs
@@ -0,0 +1,139 @@
+﻿using RimDev.Filter.Generic;
+using RimDev.Filter.Range;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Filter.Tests.Generic
+{
+    public class IEnumerable_1ExtensionsTests
+    {
+        public class Filter : IEnumerable_1ExtensionsTests
+        {
+            private class Person
+            {
+                public int FavoriteNumber { get; set; }
+                public string FirstName { get; set; }
+                public string LastName { get; set; }
+            }
+
+            private readonly IEnumerable<Person> people = new List<Person>()
+            {
+                new Person()
+                {
+                    FavoriteNumber = 5,
+                    FirstName = "John",
+                    LastName = "Doe"
+                },
+                new Person()
+                {
+                    FavoriteNumber = 10,
+                    FirstName = "Tim",
+                    LastName = "Smith"
+                },
+            };
+
+            [Fact]
+            public void Should_filter_when_property_types_match_as_constant_string()
+            {
+                var @return = people.Filter(new
+                {
+                    FirstName = "John"
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(1, @return.Count());
+                Assert.Equal("John", @return.First().FirstName);
+            }
+
+            [Fact]
+            public void Should_filter_when_property_types_match_as_constant_integer()
+            {
+                var @return = people.Filter(new
+                {
+                    FavoriteNumber = 5
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(1, @return.Count());
+                Assert.Equal("John", @return.First().FirstName);
+            }
+
+            [Fact]
+            public void Should_filter_when_property_types_match_as_enumerable_string()
+            {
+                var @return = people.Filter(new
+                {
+                    FirstName = new[] { "John" }
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(1, @return.Count());
+                Assert.Equal("John", @return.First().FirstName);
+            }
+
+            [Fact]
+            public void Should_filter_when_property_types_match_as_enumerable_integer()
+            {
+                var @return = people.Filter(new
+                {
+                    FavoriteNumber = new[] { 5 }
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(1, @return.Count());
+                Assert.Equal("John", @return.First().FirstName);
+            }
+
+            [Fact]
+            public void Should_filter_when_property_types_match_as_range_int()
+            {
+                var @return = people.Filter(new
+                {
+                    FavoriteNumber = Range.FromString<int>("[5,5]")
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(1, @return.Count());
+                Assert.Equal("John", @return.First().FirstName);
+            }
+
+            [Fact]
+            public void Should_filter_multiple_properties()
+            {
+                var @return = people.Filter(new
+                {
+                    FirstName = "John",
+                    FavoriteNumber = 0
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(0, @return.Count());
+            }
+
+            [Fact]
+            public void Should_not_throw_if_filter_does_not_contain_valid_properties()
+            {
+                var @return = people.Filter(new
+                {
+                    DOESNOTEXIST = ""
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(2, @return.Count());
+            }
+
+            [Fact]
+            public void Should_not_throw_if_filter_constant_type_does_not_match()
+            {
+                var @return = people.Filter(new
+                {
+                    FirstName = 1
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(2, @return.Count());
+            }
+        }
+    }
+}
