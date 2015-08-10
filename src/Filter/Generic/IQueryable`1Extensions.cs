4756f1ac66494eaa65cacb6e0cc09db3a2a9c17a
Justin Rusbatch <jrusbatch@gmail.com>
2015-08-10 16:24:33 -0400

    Create an IQueryable overload for Filter

---
+++ src/Filter/Generic/IQueryable`1Extensions.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using RimDev.Filter.Range.Generic;
+
+namespace RimDev.Filter.Generic
+{
+    public static class IQueryable_1Extensions
+    {
+        public static IQueryable<T> Filter<T>(
+            this IQueryable<T> value,
+            object filter)
+        {
+            if (filter == null)
+            {
+                return value;
+            }
+
+            var validValueProperties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                .Where(x => x.CanRead)
+                .ToDictionary(x => x.Name, x => x, StringComparer.OrdinalIgnoreCase);
+
+            var filterProperties = filter.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                .Where(x => x.CanRead);
+
+            var queryableValue = value;
+            foreach (var filterProperty in filterProperties)
+            {
+                PropertyInfo validValueProperty;
+                validValueProperties.TryGetValue(filterProperty.Name, out validValueProperty);
+
+                var filterPropertyValue = filterProperty.GetValue(filter, null);
+                if (validValueProperty != null && filterPropertyValue != null)
+                {
+                    var validValuePropertyName = validValueProperty.Name;
+
+                    if (typeof(IEnumerable).IsAssignableFrom(filterProperty.PropertyType) &&
+                        filterProperty.PropertyType != typeof(string))
+                    {
+                        queryableValue = queryableValue.Contains(validValuePropertyName, (IEnumerable)filterPropertyValue);
+                    }
+                    else if (filterProperty.PropertyType.IsGenericType &&
+                        typeof(IRange<>).IsAssignableFrom(filterProperty.PropertyType.GetGenericTypeDefinition()) ||
+                        filterProperty.PropertyType.GetInterfaces()
+                        .Where(x => x.IsGenericType)
+                        .Any(x => x.GetGenericTypeDefinition() == typeof(IRange<>)))
+                    {
+                        var genericTypeArgument = filterPropertyValue.GetType().GenericTypeArguments.First();
+
+                        if (genericTypeArgument == typeof(byte))
+                        {
+                            queryableValue = queryableValue.Range(validValuePropertyName, (IRange<byte>)filterPropertyValue);
+                        }
+                        else if (genericTypeArgument == typeof(char))
+                        {
+                            queryableValue = queryableValue.Range(validValuePropertyName, (IRange<char>)filterPropertyValue);
+                        }
+                        else if (genericTypeArgument == typeof(DateTime))
+                        {
+                            queryableValue = queryableValue.Range(validValuePropertyName, (IRange<DateTime>)filterPropertyValue);
+                        }
+                        else if (genericTypeArgument == typeof(decimal))
+                        {
+                            queryableValue = queryableValue.Range(validValuePropertyName, (IRange<decimal>)filterPropertyValue);
+                        }
+                        else if (genericTypeArgument == typeof(double))
+                        {
+                            queryableValue = queryableValue.Range(validValuePropertyName, (IRange<double>)filterPropertyValue);
+                        }
+                        else if (genericTypeArgument == typeof(float))
+                        {
+                            queryableValue = queryableValue.Range(validValuePropertyName, (IRange<float>)filterPropertyValue);
+                        }
+                        else if (genericTypeArgument == typeof(int))
+                        {
+                            queryableValue = queryableValue.Range(validValuePropertyName, (IRange<int>)filterPropertyValue);
+                        }
+                        else if (genericTypeArgument == typeof(long))
+                        {
+                            queryableValue = queryableValue.Range(validValuePropertyName, (IRange<long>)filterPropertyValue);
+                        }
+                        else if (genericTypeArgument == typeof(sbyte))
+                        {
+                            queryableValue = queryableValue.Range(validValuePropertyName, (IRange<sbyte>)filterPropertyValue);
+                        }
+                        else if (genericTypeArgument == typeof(short))
+                        {
+                            queryableValue = queryableValue.Range(validValuePropertyName, (IRange<short>)filterPropertyValue);
+                        }
+                        else if (genericTypeArgument == typeof(uint))
+                        {
+                            queryableValue = queryableValue.Range(validValuePropertyName, (IRange<uint>)filterPropertyValue);
+                        }
+                        else if (genericTypeArgument == typeof(ulong))
+                        {
+                            queryableValue = queryableValue.Range(validValuePropertyName, (IRange<ulong>)filterPropertyValue);
+                        }
+                        else if (genericTypeArgument == typeof(ushort))
+                        {
+                            queryableValue = queryableValue.Range(validValuePropertyName, (IRange<ushort>)filterPropertyValue);
+                        }
+                    }
+                    else
+                    {
+                        try
+                        {
+                            if (validValueProperty.PropertyType.IsGenericType &&
+                                validValueProperty.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>))
+                            {
+                                queryableValue = queryableValue.Where(validValuePropertyName, filterPropertyValue as Nullable);
+                            }
+                            else
+                            {
+                                queryableValue = queryableValue.Where(validValuePropertyName, filterPropertyValue);
+                            }
+                        }
+                        catch (Exception) { }
+                    }
+                }
+            }
+
+            return queryableValue;
+        }
+        
+        private static IQueryable<T> Contains<T>(
+            this IQueryable<T> query,
+            string property,
+            IEnumerable values)
+        {
+            if (values == null || !values.Cast<object>().Any())
+            {
+                return query;
+            }
+
+            var parameterExpression = Expression.Parameter(typeof(T), "x");
+            var propertyExpression = (Expression)Expression.Property(parameterExpression, property);
+            var constantExpression = Expression.Constant(values);
+            var propertyExpressionIsNullable = propertyExpression.Type.IsGenericType
+                                                && propertyExpression.Type.GetGenericTypeDefinition() == typeof(Nullable<>)
+                                                && constantExpression.Type.GetElementType() != propertyExpression.Type;
+
+            if (propertyExpressionIsNullable)
+            {
+                propertyExpression = Expression.Property(propertyExpression, "Value");
+            }
+
+            Expression callExpression = Expression.Call(
+                typeof(Enumerable),
+                "Contains",
+                new[] { propertyExpression.Type },
+                constantExpression,
+                propertyExpression);
+
+            if (propertyExpressionIsNullable)
+            {
+                var nullablePropertyExpression = Expression.Property(parameterExpression, property);
+                var notEqual = Expression.NotEqual(nullablePropertyExpression, Expression.Constant(null, nullablePropertyExpression.Type));
+                callExpression = Expression.AndAlso(notEqual, callExpression);
+            }
+
+            var lambda = Expression.Lambda<Func<T, bool>>(callExpression, parameterExpression);
+
+            return query.Where(lambda);
+        }
+
+        private static IQueryable<T> Where<T>(
+            this IQueryable<T> query,
+            string property,
+            object value)
+        {
+            var parameterExpression = Expression.Parameter(typeof(T), "x");
+            var propertyExpression = Expression.Property(parameterExpression, property);
+            var constantExpression = Expression.Constant(value);
+            var equalExpression = Expression.Equal(propertyExpression, constantExpression);
+            var callExpression = Expression.Call(
+                typeof(Queryable),
+                "Where",
+                new[] { query.ElementType },
+                query.Expression,
+                Expression.Lambda<Func<T, bool>>(equalExpression, parameterExpression));
+
+            return query.Provider.CreateQuery<T>(callExpression);
+        }
+
+        private static IQueryable<T> Range<T, TRange>(
+            this IQueryable<T> query,
+            string property,
+            IRange<TRange> range)
+            where TRange : struct
+        {
+            var parameterExpression = Expression.Parameter(typeof(T), "x");
+            var propertyExpression = Expression.Property(parameterExpression, property);
+
+            Expression minConstantExpression = null;
+
+            if (range.MinValue.HasValue)
+            {
+                var minValueExpression = Expression.Constant(range.MinValue);
+                minConstantExpression = minValueExpression.Type != propertyExpression.Type
+                    ? Expression.Convert(minValueExpression, propertyExpression.Type)
+                    : (Expression)minValueExpression;
+            }
+
+            BinaryExpression minGreaterExpression = null;
+
+            if (minConstantExpression != null)
+            {
+                minGreaterExpression = range.IsMinInclusive
+                ? Expression.GreaterThanOrEqual(propertyExpression, minConstantExpression)
+                : Expression.GreaterThan(propertyExpression, minConstantExpression);
+            }
+
+            Expression maxConstantExpression = null;
+
+            if (range.MaxValue.HasValue)
+            {
+                var maxValueExpression = Expression.Constant(range.MaxValue);
+                maxConstantExpression = maxValueExpression.Type != propertyExpression.Type
+                    ? Expression.Convert(maxValueExpression, propertyExpression.Type)
+                    : (Expression)maxValueExpression;
+            }
+
+            BinaryExpression maxLessExpression = null;
+
+            if (maxConstantExpression != null)
+            {
+                maxLessExpression = range.IsMaxInclusive
+                ? Expression.LessThanOrEqual(propertyExpression, maxConstantExpression)
+                : Expression.LessThan(propertyExpression, maxConstantExpression);
+            }
+
+            Expression logicExpression;
+
+            if (minGreaterExpression != null && maxLessExpression != null)
+            {
+                logicExpression = Expression.And(minGreaterExpression, maxLessExpression);
+            }
+            else if (minGreaterExpression != null)
+            {
+                logicExpression = minGreaterExpression;
+            }
+            else
+            {
+                logicExpression = maxLessExpression;
+            }
+
+            var lambdaExpression = Expression.Lambda<Func<T, bool>>(logicExpression, parameterExpression);
+            var callExpression = Expression.Call(
+                typeof(Queryable),
+                "Where",
+                new[] { query.ElementType },
+                query.Expression,
+                lambdaExpression);
+
+            return query.Provider.CreateQuery<T>(callExpression);
+        }
+    }
+}
---
+++ tests/Filter.Tests/Generic/IQueryable`1ExtensionsTests.cs
@@ -0,0 +1,432 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using RimDev.Filter.Range.Generic;
+using RimDev.Filter.Generic;
+using Xunit;
+
+namespace RimDev.Filter.Tests.Generic
+{
+    public class IQueryable_1ExtensionsTests
+    {
+        public class Filter : IQueryable_1ExtensionsTests
+        {
+            public class Person
+            {
+                public DateTime FavoriteDate { get; set; }
+                public char FavoriteLetter { get; set; }
+                public int FavoriteNumber { get; set; }
+                public string FirstName { get; set; }
+                public string LastName { get; set; }
+                public decimal? Rating { get; set; }
+            }
+
+            protected readonly IQueryable<Person> People = new List<Person>()
+            {
+                new Person()
+                {
+                    FavoriteDate = DateTime.Parse("2000-01-01"),
+                    FavoriteLetter = 'a',
+                    FavoriteNumber = 5,
+                    FirstName = "John",
+                    LastName = "Doe"
+                },
+                new Person()
+                {
+                    FavoriteDate = DateTime.Parse("2000-01-02"),
+                    FavoriteLetter = 'b',
+                    FavoriteNumber = 10,
+                    FirstName = "Tim",
+                    LastName = "Smith",
+                    Rating = 4.5m
+                },
+            }.AsQueryable();
+        }
+
+        public class ConstantFilters : Filter
+        {
+            [Fact]
+            public void Should_filter_when_property_types_match_as_constant_string()
+            {
+                var @return = People.Filter(new
+                {
+                    FirstName = "John"
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(1, @return.Count());
+                Assert.Equal("John", @return.First().FirstName);
+            }
+
+            [Fact]
+            public void Should_filter_when_property_types_match_as_constant_integer()
+            {
+                var @return = People.Filter(new
+                {
+                    FavoriteNumber = 5
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(1, @return.Count());
+                Assert.Equal("John", @return.First().FirstName);
+            }
+
+            [Fact]
+            public void Should_filter_multiple_properties()
+            {
+                var @return = People.Filter(new
+                {
+                    FirstName = "John",
+                    FavoriteNumber = 0
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(0, @return.Count());
+            }
+
+            [Fact]
+            public void Should_not_throw_if_filter_does_not_contain_valid_properties()
+            {
+                var @return = People.Filter(new
+                {
+                    DOESNOTEXIST = ""
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(2, @return.Count());
+            }
+
+            [Fact]
+            public void Should_not_throw_if_filter_constant_type_does_not_match()
+            {
+                var @return = People.Filter(new
+                {
+                    FirstName = 1
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(2, @return.Count());
+            }
+        }
+
+        public class EnumerableFilters : Filter
+        {
+            [Fact]
+            public void Should_bypass_filter_when_empty_collection()
+            {
+                var @return = People.Filter(new
+                {
+                    FirstName = new string[] { }
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(2, @return.Count());
+            }
+
+            [Fact]
+            public void Should_filter_when_property_types_match_as_enumerable_string()
+            {
+                var @return = People.Filter(new
+                {
+                    FirstName = new[] { "John" }
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(1, @return.Count());
+                Assert.Equal("John", @return.First().FirstName);
+            }
+
+            [Fact]
+            public void Should_filter_when_property_types_match_as_enumerable_integer()
+            {
+                var @return = People.Filter(new
+                {
+                    FavoriteNumber = new[] { 5 }
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(1, @return.Count());
+                Assert.Equal("John", @return.First().FirstName);
+            }
+
+            [Fact]
+            public void Should_be_able_to_handle_nullable_destination_and_primitive_filter()
+            {
+                var @return = People.Filter(new
+                {
+                    Rating = new[] { 4.5m }
+                });
+
+                Assert.Equal(1, @return.Count());
+            }
+
+            [Fact]
+            public void Should_be_able_to_handle_nullable_destination_and_nullable_primitive_filter_as_collection()
+            {
+                var @return = People.Filter(new
+                {
+                    Rating = new decimal?[] { 4.5m }
+                });
+
+                Assert.Equal(1, @return.Count());
+            }
+
+            [Fact]
+            public void Should_be_able_to_handle_nullable_destination_and_nullable_primitive_filter()
+            {
+                var @return = People.Filter(new
+                {
+                    Rating = new decimal?(4.5m)
+                });
+
+                Assert.Equal(1, @return.Count());
+            }
+
+            [Fact]
+            public void Should_be_able_to_handle_nullable_source()
+            {
+                var people = People.ToList();
+                people.ForEach(x => x.Rating = null);
+
+                var @return = people.Filter(new
+                {
+                    Rating = new decimal?[] { 4.5m }
+                });
+
+                Assert.Equal(0, @return.Count());
+            }
+        }
+
+        public class RangeFilters : Filter
+        {
+            [Theory,
+            InlineData("(,5]"),
+            InlineData("(-∞,5]")]
+            public void Should_filter_open_ended_lower_bound(string value)
+            {
+                var @return = People.Filter(new
+                {
+                    FavoriteNumber = Range.Range.FromString<int>(value)
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(1, @return.Count());
+                Assert.Equal("John", @return.First().FirstName);
+            }
+
+            [Theory,
+            InlineData("(5,]"),
+            InlineData("(5,+∞)")]
+            public void Should_filter_open_ended_upper_bound(string value)
+            {
+                var @return = People.Filter(new
+                {
+                    FavoriteNumber = Range.Range.FromString<int>(value)
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(1, @return.Count());
+                Assert.Equal("Tim", @return.First().FirstName);
+            }
+
+            [Fact]
+            public void Should_filter_for_concrete_range()
+            {
+                var @return = People.Filter(new
+                {
+                    FavoriteNumber = new Range<int>()
+                    {
+                        MinValue = 5,
+                        MaxValue = 5,
+                        IsMinInclusive = true,
+                        IsMaxInclusive = true
+                    }
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(1, @return.Count());
+                Assert.Equal("John", @return.First().FirstName);
+            }
+
+            [Fact]
+            public void Should_filter_when_property_types_match_as_range_byte()
+            {
+                var @return = People.Filter(new
+                {
+                    FavoriteNumber = Range.Range.FromString<byte>("[5,5]")
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(1, @return.Count());
+                Assert.Equal("John", @return.First().FirstName);
+            }
+
+            [Fact]
+            public void Should_filter_when_property_types_match_as_range_char()
+            {
+                var @return = People.Filter(new
+                {
+                    FavoriteLetter = Range.Range.FromString<char>("[a,b)")
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(1, @return.Count());
+                Assert.Equal("John", @return.First().FirstName);
+            }
+
+            [Fact]
+            public void Should_filter_when_property_types_match_as_range_datetime()
+            {
+                var @return = People.Filter(new
+                {
+                    FavoriteDate = Range.Range.FromString<DateTime>("[2000-01-01,2000-01-02)")
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(1, @return.Count());
+                Assert.Equal("John", @return.First().FirstName);
+            }
+
+            [Fact]
+            public void Should_filter_when_property_types_match_as_range_decimal()
+            {
+                var @return = People.Filter(new
+                {
+                    FavoriteNumber = Range.Range.FromString<decimal>("[4.5,5]")
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(1, @return.Count());
+                Assert.Equal("John", @return.First().FirstName);
+            }
+
+            [Fact]
+            public void Should_filter_when_property_types_match_as_range_double()
+            {
+                var @return = People.Filter(new
+                {
+                    FavoriteNumber = Range.Range.FromString<double>("[4.5,5]")
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(1, @return.Count());
+                Assert.Equal("John", @return.First().FirstName);
+            }
+
+            [Fact]
+            public void Should_filter_when_property_types_match_as_range_float()
+            {
+                var @return = People.Filter(new
+                {
+                    FavoriteNumber = Range.Range.FromString<float>("[4.5,5]")
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(1, @return.Count());
+                Assert.Equal("John", @return.First().FirstName);
+            }
+
+            [Fact]
+            public void Should_filter_when_property_types_match_as_range_int()
+            {
+                var @return = People.Filter(new
+                {
+                    FavoriteNumber = Range.Range.FromString<int>("[5,5]")
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(1, @return.Count());
+                Assert.Equal("John", @return.First().FirstName);
+            }
+
+            [Fact]
+            public void Should_filter_when_property_types_match_as_range_long()
+            {
+                var @return = People.Filter(new
+                {
+                    FavoriteNumber = Range.Range.FromString<long>("[5,5]")
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(1, @return.Count());
+                Assert.Equal("John", @return.First().FirstName);
+            }
+
+            [Fact]
+            public void Should_filter_when_property_types_match_as_range_sbyte()
+            {
+                var @return = People.Filter(new
+                {
+                    FavoriteNumber = Range.Range.FromString<sbyte>("[5,5]")
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(1, @return.Count());
+                Assert.Equal("John", @return.First().FirstName);
+            }
+
+            [Fact]
+            public void Should_filter_when_property_types_match_as_range_short()
+            {
+                var @return = People.Filter(new
+                {
+                    FavoriteNumber = Range.Range.FromString<short>("[5,5]")
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(1, @return.Count());
+                Assert.Equal("John", @return.First().FirstName);
+            }
+
+            [Fact]
+            public void Should_filter_when_property_types_match_as_range_uint()
+            {
+                var @return = People.Filter(new
+                {
+                    FavoriteNumber = Range.Range.FromString<uint>("[5,5]")
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(1, @return.Count());
+                Assert.Equal("John", @return.First().FirstName);
+            }
+
+            [Fact]
+            public void Should_filter_when_property_types_match_as_range_ulong()
+            {
+                var @return = People.Filter(new
+                {
+                    FavoriteNumber = Range.Range.FromString<ulong>("[5,5]")
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(1, @return.Count());
+                Assert.Equal("John", @return.First().FirstName);
+            }
+
+            [Fact]
+            public void Should_filter_when_property_types_match_as_range_ushort()
+            {
+                var @return = People.Filter(new
+                {
+                    FavoriteNumber = Range.Range.FromString<ushort>("[5,5]")
+                });
+
+                Assert.NotNull(@return);
+                Assert.Equal(1, @return.Count());
+                Assert.Equal("John", @return.First().FirstName);
+            }
+
+            [Fact]
+            public void Should_be_able_to_handle_nullable_source()
+            {
+                var @return = People.Filter(new
+                {
+                    Rating = (Range<decimal>)"[4.5,5.0]"
+                });
+
+                Assert.Equal(1, @return.Count());
+            }
+        }
+    }
+}
