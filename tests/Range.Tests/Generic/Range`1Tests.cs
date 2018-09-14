6c082304638316a5461ece49f4dbfbb25aa811d7
Khalid Abuhakmeh <khalid@aquabirdconsulting.com>
2018-09-14 14:56:53 -0400

    Can determine if any object is a range of numeric or date

--- tests/Range.Tests/Generic/Range`1Tests.cs
+++ tests/Range.Tests/Generic/Range`1Tests.cs
@@ -25,29 +25,19 @@ namespace RimDev.Filter.Range.Tests.Generic
             Assert.True(sut.IsMinInclusive);
             Assert.True(sut.IsMaxInclusive);
         }
-    
+        
         [Fact]
-        public void Should_determine_range_as_date_range()
+        public void Should_determine_object_as_date_range()
         {
-            var dateTimeRange = new Range<DateTime>();
-            var dateTimeOffsetRange = new Range<DateTimeOffset>();
-            var intRange = new Range<int>();
-
-            Assert.True(dateTimeRange.IsDateRange());
-            Assert.True(dateTimeOffsetRange.IsDateRange());
-            Assert.False(intRange.IsDateRange());
+            var dateTimeRange = new Range<DateTime>() as object;
+            Assert.True(Range.IsDateRange(dateTimeRange));
         }
         
         [Fact]
-        public void Should_determine_range_as_numeric_range()
+        public void Should_determine_object_as_numeric_range()
         {
-            var floatRange = new Range<float>();
-            var intRange = new Range<int>();
-            var dateRange = new Range<DateTime>();
-
-            Assert.True(floatRange.IsNumericRange());
-            Assert.True(intRange.IsNumericRange());
-            Assert.False(dateRange.IsNumericRange());
+            var numericRange = new Range<int>() as object;
+            Assert.True(Range.IsNumericRange(numericRange));
         }
     }
 }
