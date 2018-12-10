4843e168718095d90927175f88a32fef4582e5fc
Bill Boga <bill.boga@ritterim.com>
2018-12-10 09:55:33 -0500

    Include NPoco project in build
    Previous behavior did not generate NuGet package.

--- src/Filter.NPoco/Properties/AssemblyInfo.cs
+++ src/Filter.NPoco/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("36314f91-8877-4223-a1d2-0af23b92a086")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
