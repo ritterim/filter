8529049a44a364a0128151dada412c5ea023eb50
Thomas Harold <tom.harold@ritterim.com>
2021-10-26 10:49:33 -0400

    Rework configuration to play nicer with Docker
    By splitting up some of the configuration settings, it is
    easier to use custom ports when setting up the ES/SQL
    Docker containers.

--- tests/Filter.Tests.Common/Testing/Configuration/RimDevTestsElasticsearchConfiguration.cs
+++ tests/Filter.Tests.Common/Testing/Configuration/RimDevTestsElasticsearchConfiguration.cs
@@ -4,6 +4,31 @@ namespace Filter.Tests.Common.Testing.Configuration
 {
     public class RimDevTestsElasticsearchConfiguration
     {
-        public Uri Uri { get; set; } = new Uri("http://localhost:9201/");
+        /// <summary>Port number (host.port) for the Elasticsearch instance.  It's kept separate
+        /// in the C# configuration to make things easier in cases where the build script is
+        /// spinning up a Docker instance.
+        /// </summary>
+        public int? Port { get; set; } = 9200;
+        
+        /// <summary>Port number (transport.port) for node communication within the Elasticsearch
+        /// instance. This is no longer used by the client libraries when talking to Elasticsearch
+        /// but is still required to be specified on the Docker configuration.
+        /// </summary>
+        public int? TransportPort { get; set; } = 9300;
+        
+        /// <summary>The base URI for talking to the Elasticsearch instance in tests.
+        /// This gets combined with Port (if specified) to form the final URI. 
+        /// </summary>
+        public Uri BaseUri { get; set; } = new Uri("http://localhost/");
+
+        /// <summary>URI to use when tests access the Elasticsearch instance.</summary>
+        public Uri Uri {
+            get
+            {
+                if (Port is null || Port == BaseUri.Port) return BaseUri;
+                var uriBuilder = new UriBuilder(BaseUri) { Port = Port.Value };
+                return uriBuilder.Uri;
+            }
+        }
     }
 }
--- tests/Filter.Tests.Common/Testing/Configuration/RimDevTestsSqlConfiguration.cs
+++ tests/Filter.Tests.Common/Testing/Configuration/RimDevTestsSqlConfiguration.cs
@@ -2,9 +2,16 @@ namespace Filter.Tests.Common.Testing.Configuration
 {
     public class RimDevTestsSqlConfiguration
     {
-        public string DataSource { get; set; } = "localhost,1433";
+        public string Hostname { get; set; } = "localhost";
+        
+        /// <summary>Port that SQL Server will be listening on.  This is kept separate for ease of
+        /// use in the build scripts if Docker SQL is used.</summary>
+        public int? Port { get; set; } = 1433;
+        
         public string InitialCatalog { get; set; } = "master";
         public string UserId { get; set; } = "sa";
         public string Password { get; set; }
+        
+        public string DataSource => $"{Hostname},{Port}";
     }
 }
